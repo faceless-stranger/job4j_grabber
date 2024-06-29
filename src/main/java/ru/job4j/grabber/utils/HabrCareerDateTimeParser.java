a2a8698337c6e9587b7b18545a47e83744b55039
PC <faceless-stranger@mail.ru>
2024-06-29 18:44:49 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,23 +1,18 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        String trimmedDateTime = parse.substring(0, parse.indexOf('+'));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
-        LocalDateTime localDateTime = LocalDateTime.parse(trimmedDateTime, formatter);
-        System.out.println(localDateTime);
+        // Преобразуем строку в OffsetDateTime, который учитывает временную зону
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(parse);
+        // Преобразуем OffsetDateTime в LocalDateTime, который не учитывает временную зону
+        LocalDateTime localDateTime = offsetDateTime.toLocalDateTime();
+        // Выводим результат для проверки
         return localDateTime;
     }
-
-    public static void main(String[] args) {
-        String test = "2024-06-27T12:16:02+03:00";
-        HabrCareerDateTimeParser habrCareerDateTimeParser = new HabrCareerDateTimeParser();
-        habrCareerDateTimeParser.parse(test);
-    }
-
 }
---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void testParseValidDateTimeString() {
+        String dateTimeString = "2024-06-29T15:30:00+03:00";
+        LocalDateTime expected = LocalDateTime.of(2024, 6, 29, 15, 30, 0);
+
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime result = parser.parse(dateTimeString);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testParseDifferentDateTimeString() {
+        String dateTimeString = "2023-12-25T10:45:30+01:00";
+        LocalDateTime expected = LocalDateTime.of(2023, 12, 25, 10, 45, 30);
+
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime result = parser.parse(dateTimeString);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testParseDateTimeWithZeroOffset() {
+        String dateTimeString = "2022-01-01T12:00:00Z";
+        LocalDateTime expected = LocalDateTime.of(2022, 1, 1, 12, 0, 0);
+
+        HabrCareerDateTimeParser parser = new HabrCareerDateTimeParser();
+        LocalDateTime result = parser.parse(dateTimeString);
+
+        assertEquals(expected, result);
+    }
+}
